7a8927d2b7f386d6cc08b154ec322556046a2dee
sebastian rey <182707930+Sebas-ti@users.noreply.github.com>
2025-03-04 13:47:59 -0500

    Cree y agregue los statementes de sql

---
+++ statements/01_slect.sql
@@ -0,0 +1,7 @@
+#SELECT nos sirve para consultar y recuperar datos de la base de datos#
+#FROM nos sirve para sleccionar la base de datos#
+
+SELECT * FROM users 
+SELECT name FROM users;
+SELECT name,last_name FROM users;
+
---
+++ statements/02_distinct.sql
@@ -0,0 +1,7 @@
+#DISTINCT se utiliza para eliminar duplicados y devolver filas únicas en el resultado de una consulta
+
+para llamar seria 
+SELECT//DISTINCT//columna//FROM//tabla
+
+SELECT DISTINCT * FROM users
+SELECT DISTINCT age FROM users
